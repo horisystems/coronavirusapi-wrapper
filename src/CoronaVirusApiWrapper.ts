a27a0b4a278fb9acdc2a8b7b2853c6137566a0e7
Finbarrs Oketunji <f@finbarrs.eu>
2022-03-02 23:58:47 +0000

    v2.2.5
    Signed-off-by: Finbarrs Oketunji <f@finbarrs.eu>

--- src/CoronaVirusApiWrapper.ts
+++ src/CoronaVirusApiWrapper.ts
@@ -184,6 +184,10 @@ export default class CoronaVirusApiWrapper {
     return this.getReportsAt(new Date(Date.parse("feb-2022")));
   }
 
+  getMar2022(): Promise<IGlobalDailyReport | Error> {
+    return this.getReportsAt(new Date(Date.parse("mar-2022")));
+  }
+
   async getTimeSeriesFor(
     type: TimeSeriesType,
     country: TimeSeriesCountry
